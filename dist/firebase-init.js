e0be1e6db5210d8f52fdb733b9c1723c07b03c74
solangealmn <solangealmn@gmail.com>
2018-11-08 19:02:58 -0200

    rotas implementadas

---
+++ dist/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD71dYUcPQNioXuMWCJo5DLe45-CZot7q4",
+  authDomain: "app-fcamara.firebaseapp.com",
+  databaseURL: "https://app-fcamara.firebaseio.com",
+  projectId: "app-fcamara",
+  storageBucket: "app-fcamara.appspot.com",
+  messagingSenderId: "382435328021"
+};
+firebase.initializeApp(config);
