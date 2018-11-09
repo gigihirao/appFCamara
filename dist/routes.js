ae4d47afea72f60aa638424155a220db662e8435
solangealmn <solangealmn@gmail.com>
2018-11-09 05:26:39 -0200

    erro do pages.js corrigido

--- dist/routes.js
+++ dist/routes.js
@@ -1,21 +1,30 @@
+
 page('/', home);
 page('/clients', clients);
-page('/sellers', sellers)
-page('/orders', orders)
-page('/products', products)
-page();
+page('/sellers', sellers);
+page('/orders', orders);
+page('/products', products);
+
+//Erro no Pages.js reportado na issues (https://github.com/visionmedia/page.js/issues/522)
+page({ dispatch: false });
+
+
 function home() {
   $('#main').html("gráficos totais")
 }
+
 function clients() {
   $('#main').html("gráficos clientes")
 }
+
 function sellers() {
   $('#main').html("gráficos vendedores")
 }
+
 function orders() {
   $('#main').html("gráficos orders")
 }
+
 function products() {
   $('#main').html("gráficos produtos")
 }
